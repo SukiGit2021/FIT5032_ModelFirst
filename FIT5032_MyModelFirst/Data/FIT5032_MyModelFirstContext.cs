2962d46dc24fb89c1de2275ee5c07e7b3f4a6be7
SukiGit2021 <pliu0030@student.monash.edu>
2021-08-18 18:57:38 +1000

    Add project files.

---
+++ FIT5032_MyModelFirst/Data/FIT5032_MyModelFirstContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_MyModelFirst.Data
+{
+    public class FIT5032_MyModelFirstContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public FIT5032_MyModelFirstContext() : base("name=FIT5032_MyModelFirstContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<FIT5032_MyModelFirst.Models.Student> Students { get; set; }
+    }
+}
